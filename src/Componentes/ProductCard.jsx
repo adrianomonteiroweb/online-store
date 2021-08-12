255f2cff982851c14e99f933f4affe19d29e18a3
suco <francalucas937@gmail.com>
2021-08-12 19:16:56 -0300

    resolve conflits

--- src/Componentes/ProductCard.jsx
+++ src/Componentes/ProductCard.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import PropTypes from 'prop-types';
 // import { Link } from 'react-router-dom';
 import './ProductCard.css';
-
 // import BotoesDosProdutos from './BotoesDosProdutos';
 import FreteComponent from './FreteComponent';
 
