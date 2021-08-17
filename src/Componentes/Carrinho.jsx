5f5054fb3890918a8c5129af37fe1d0aa4584c4c
suco <francalucas937@gmail.com>
2021-08-17 12:10:01 -0300

    resolvendo problema ao remover items

--- src/Componentes/Carrinho.jsx
+++ src/Componentes/Carrinho.jsx
@@ -1,64 +1,72 @@
 import React from 'react';
-// import Loading from './Loading';
+import Loading from './Loading';
 // import { getProductsFromCategoryAndQuery } from '../services/api';
 import ProductCartCard from './ProductCartCard';
 
 class Carrinho extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      listaDeProdutos: [],
-      loading: true,
-    };
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			listaDeProdutos: [],
+			loading: true,
+		};
+	}
 
-  componentDidMount() {
-    this.getStorageItems();
-  }
+	componentDidMount() {
+		this.getStorageItems();
+	}
 
-  getStorageItems = () => {
-    // const { listaDeProdutos } = this.state;
-    const localStorageItems = JSON.parse(localStorage.getItem('items'));
-    if (localStorageItems !== null) {
-      this.setState({ listaDeProdutos: [...localStorageItems] });
-      this.setState({ loading: false });
-    } else {
-      this.setState({ loading: true });
-    }
-  };
+	getStorageItems = () => {
+		const localStorageItems = JSON.parse(localStorage.getItem('items'));
+		// console.log(localStorageItems.length)
+		if (localStorage.getItem('items').length !== 2) {
+			this.setState({ listaDeProdutos: [...localStorageItems] });
+			this.setState({ loading: false });
+			console.log(localStorageItems)
+		}
+	};
 
-  remover = (id) => {
-    const { listaDeProdutos } = this.state;
-    const resultadoBusca = listaDeProdutos.find((elemento) => elemento.id === id);
-    const indexARetirar = listaDeProdutos
-      .indexOf(resultadoBusca);
-    listaDeProdutos.splice(indexARetirar, 1);
-    this.setState({ listaDeProdutos });
-  }
+	remover = (id) => {
+		const { listaDeProdutos } = this.state;
+		const findCartItem = listaDeProdutos.find((elemento) => elemento.id === id);
+		
+		const localStorageItems = JSON.parse(localStorage.getItem('items'));
+		const filtered = localStorageItems.filter(item => item.id !== id);
+		localStorage.setItem('items', JSON.stringify(filtered));
 
-  render() {
-    const { listaDeProdutos, loading } = this.state;
+		const indexARetirar = listaDeProdutos
+			.indexOf(findCartItem);
+		listaDeProdutos.splice(indexARetirar, 1);
+		this.setState({ listaDeProdutos });
+    
+		if (localStorage.getItem('items').length === 2) {
+			this.setState({ loading: true });
+		}
+	}
 
-    // if (loading) return <Loading />;
+	render() {
+		const { listaDeProdutos, loading } = this.state;
+    // console.log(listaDeProdutos)
+		// if (loading) return <Loading />;
 
-    if (loading) {
-      return (
-        <p data-testid="shopping-cart-empty-message">Seu carrinho está vazio</p>
-      );
-    }
-    return (
-      <div>
-        {listaDeProdutos.map((produto) => (
-          <div key={ produto.id } className="containerItem">
-            <ProductCartCard
-              key={ produto.id }
-              product={ produto }
-              funcRemover={ this.remover }
-            />
-          </div>))}
-      </div>
-    );
-  }
+		if (loading) {
+			return (
+				<p data-testid="shopping-cart-empty-message">Seu carrinho está vazio</p>
+			);
+		}
+		return (
+			<div>
+				{listaDeProdutos.map((produto) => (
+					<div key={ produto.id } className="containerItem">
+						<ProductCartCard
+							key={ produto.id }
+							product={ produto }
+							funcRemover={ this.remover }
+						/>
+					</div>))}
+			</div>
+		);
+	}
 }
 
 export default Carrinho;
